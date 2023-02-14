b2db1bb211deb95f4b22f4f3c03cdefb389d8aa0
NikolasZR <nicolasaberastegui97@gmail.com>
2023-02-14 18:16:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Imagenes from './Components/Imagenes'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+    <h1 >Te Amo Princesa</h1>
+      <Imagenes />
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/Components/Imagenes.jsx
@@ -0,0 +1,49 @@
+
+
+import img1 from '../Images/1.jpeg';
+import img2 from '../Images/2.jpeg';
+import img3 from '../Images/3.jpeg';
+import img4 from '../Images/4.jpeg';
+import img5 from '../Images/5.jpeg';
+import img6 from '../Images/6.jpeg';
+import img7 from '../Images/7.jpeg';
+import img8 from '../Images/8.jpeg';
+import img9 from '../Images/9.jpeg';
+import img10 from '../Images/10.jpeg';
+import img11 from '../Images/11.jpeg';
+import img12 from '../Images/12.jpeg';
+import img13 from '../Images/13.jpeg';
+import img14 from '../Images/14.jpeg';
+import img15 from '../Images/15.jpeg';
+import img16 from '../Images/16.jpeg';
+import img17 from '../Images/17.jpeg';
+import './imagenes.css'
+
+const Imagenes = () => {
+
+    return (
+        <section >
+            <img src={img1} alt="img1" className="img-fluid" />
+            <img src={img2} alt="img2" className="img-fluid" />
+            <img src={img3} alt="img3" className="img-fluid" />
+            <img src={img4} alt="img4" className="img-fluid" />
+            <img src={img5} alt="img5" className="img-fluid" />
+            <img src={img6} alt="img6" className="img-fluid" />
+            <img src={img7} alt="img7" className="img-fluid" />
+            <img src={img8} alt="img8" className="img-fluid" />
+            <img src={img9} alt="img9" className="img-fluid" />
+            <img src={img10} alt="img10" className="img-fluid" />
+            <img src={img11} alt="img11" className="img-fluid" />
+            <img src={img12} alt="img12" className="img-fluid" />
+            <img src={img13} alt="img13" className="img-fluid" />
+            <img src={img14} alt="img14" className="img-fluid" />
+            <img src={img15} alt="img15" className="img-fluid" />
+            <img src={img16} alt="img16" className="img-fluid" />
+            <img src={img17} alt="img17" className="img-fluid" />
+        </section>
+
+
+    );
+}
+
+export default Imagenes;
